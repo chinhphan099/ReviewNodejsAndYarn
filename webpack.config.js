33c496ba0ce975eb15e2f812caa195c3819258de
Chinh Phan <chinhphan099@gmail.com>
2023-03-13 20:56:20 -0400

    env

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,46 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development', // development, production, none
-  entry: {
-    app: path.resolve('src/index.js') // Tên file là app.js
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss|css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: 'src/template.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: 'dist'
+module.exports = (env) => {
+  const isDevelopment = Boolean(env.development)
+  return {
+    mode: isDevelopment ? 'development' : 'production', // development, production, none
+    entry: {
+      app: path.resolve('src/index.js')
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true
+    },
+    devtool: isDevelopment ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Webpack App',
+        filename: 'index.html',
+        template: 'src/template.html'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: 'dist'
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true
+    }
   }
 }
